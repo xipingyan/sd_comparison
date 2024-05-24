0750de51a3759b1a6f47b0bdd801ec119590eedf
Xiping Yan <xiping.yan@intel.com>
2024-05-24 16:54:20 +0800

    Update sd_2_1_ov.py

--- sd_2_1_ov.py
+++ sd_2_1_ov.py
@@ -17,7 +17,7 @@ def print_ov_version():
     print("==========================")
 
 
-def test_sd_2_1_ov(model_id, prompt, width, height, nsteps, loop_num, enable_bf16:bool):
+def test_sd_2_1_ov(model_id, prompt, width, height, nsteps, loop_num, enable_bf16:bool, is_pt_model:bool):
     print("\n*********************************************************")
     print_ov_version()
     stm = StatisticTM("Test SD 2.1 with OV")
@@ -30,20 +30,24 @@ def test_sd_2_1_ov(model_id, prompt, width, height, nsteps, loop_num, enable_bf1
         return
     print(f"  Test pytorch model: {model_id}")
 
-    saved_ov_model="./ov_model"
     if enable_bf16:
         ov_cfg={"INFERENCE_PRECISION_HINT":"bf16"}
     else:
         ov_cfg={"INFERENCE_PRECISION_HINT":"f32"}
-
+    print(f"  ov_cfg={ov_cfg}")
     print(f"  height={height}, width={width}.")
-    if not os.path.exists(saved_ov_model):
-        ov_pipe = OVStableDiffusionPipeline.from_pretrained(model_id, export=True)
-        ov_pipe(prompt, num_inference_steps=nsteps, height=height, width=width, output_type="numpy")
-        ov_pipe.save_pretrained(saved_ov_model)
-        print(f"== Test pytorch model: {model_id}")
+
+    print(f"  is_pt_model={is_pt_model}"
+    if is_pt_model:
+        saved_ov_model="./ov_model"
+        if not os.path.exists(saved_ov_model):
+            ov_pipe = OVStableDiffusionPipeline.from_pretrained(model_id, export=True)
+            ov_pipe(prompt, num_inference_steps=nsteps, height=height, width=width, output_type="numpy")
+            ov_pipe.save_pretrained(saved_ov_model)
+            model_id=saved_ov_model
+            print(f"== Test pytorch model: {model_id}")
     else:
-        ov_pipe = OVStableDiffusionPipeline.from_pretrained(saved_ov_model, ov_config=ov_cfg)
+        ov_pipe = OVStableDiffusionPipeline.from_pretrained(model_id, ov_config=ov_cfg)
 
     ov_pipe.reshape(batch_size=1, height=height, width=width, num_images_per_prompt=1)
 
@@ -55,7 +59,8 @@ def test_sd_2_1_ov(model_id, prompt, width, height, nsteps, loop_num, enable_bf1
     elapsed_time(ov_pipe, prompt, width, height, None, 1, 1)
 
     # inference.
-    stm = elapsed_time(ov_pipe, prompt, width, height, loop_num, nsteps, saved_img_fn="rslt_sd2_1_ov.png")
+    stm = []
+    stm = elapsed_time(ov_pipe, prompt, width, height, stm, loop_num, nsteps, saved_img_fn="rslt_sd2_1_ov.png")
     print(stm)
     return stm
 
